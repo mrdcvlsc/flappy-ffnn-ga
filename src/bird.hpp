78acbe00c935575d7263a06eff1bd559eb2483fd
mrdcvlsc <mrdcvlsc@gmail.com>
2023-10-18 21:56:20 +0800

    created the bird class

--- src/bird.hpp
+++ src/bird.hpp
@@ -1,6 +1,26 @@
 #ifndef MRDCVLSC_BIRD_HPP
 #define MRDCVLSC_BIRD_HPP
 
+#include "config.hpp"
+#include <SFML/Graphics/RectangleShape.hpp>
 
+struct Bird : public sf::RectangleShape {
+    /// Default bird side dimension length (SIZE x SIZE).
+    static constexpr float SIZE = 20.f;
+
+    /// Starting x position of the bird in the 2D world.
+    static constexpr float START_X_POS = static_cast<float>(WINDOW_WIDTH) / 5.f;
+
+    /// Starting y position of the bird in the 2D world.
+    static constexpr float START_Y_POS = static_cast<float>(WINDOW_HEIGHT) / 2.f;
+
+    Bird() : sf::RectangleShape({SIZE, SIZE}) {
+        setOrigin(getSize() / 2.f);
+        setFillColor(sf::Color::Red);
+        setOutlineThickness(3.f);
+        setOutlineColor(sf::Color::Black);
+        setPosition(START_X_POS, START_Y_POS);
+    }
+};
 
 #endif
--- src/config.hpp
+++ src/config.hpp
@@ -4,13 +4,9 @@
 #include <cstddef>
 #include <cinttypes>
 
-static constexpr size_t FPS_LIMIT = 60;
+static constexpr float VIEW_MOVE_DISTANCE = 2.f;
 
 static constexpr size_t WINDOW_WIDTH = 640;
 static constexpr size_t WINDOW_HEIGHT = 360;
-
-static constexpr float BIRD_SIZE = 20.f;
-static constexpr float BIRD_STARTING_X_POS = static_cast<float>(WINDOW_WIDTH) / 4.f;
-static constexpr float BIRD_STARTING_Y_POS = static_cast<float>(WINDOW_HEIGHT) / 2.f;
 
 #endif
