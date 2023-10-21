267d12d58a4b9eaae0dc77f9bc30e4b090d9e042
mrdcvlsc <mrdcvlsc@gmail.com>
2023-10-21 11:38:15 +0800

    implement jump for Bird class

--- src/bird.hpp
+++ src/bird.hpp
@@ -1,6 +1,8 @@
 #ifndef MRDCVLSC_BIRD_HPP
 #define MRDCVLSC_BIRD_HPP
 
+#include <iostream>
+
 #include <SFML/Graphics/RectangleShape.hpp>
 
 #include "config.hpp"
@@ -13,14 +15,53 @@ struct Bird : public sf::RectangleShape {
     static constexpr float START_X_POS = static_cast<float>(WINDOW_WIDTH) / 5.f;
 
     /// Starting y position of the bird in the 2D world.
-    static constexpr float START_Y_POS = static_cast<float>(WINDOW_HEIGHT) / 2.f;
+    static constexpr float START_Y_POS = static_cast<float>(WINDOW_HEIGHT) * 0.5f;
 
-    Bird() : sf::RectangleShape({SIZE, SIZE}) {
-        setOrigin(getSize() / 2.f);
+    /// Bird initial vertical speed.
+    static constexpr float JUMP_SPEED = -260.f;
+
+    /// Gravitation acceleration.
+    static constexpr float GRAVITY = 570.f;
+
+    float time_lived;
+    float speed;
+
+    Bird() : sf::RectangleShape({SIZE, SIZE}), time_lived(0.f), speed(JUMP_SPEED * 0.75f) {
+        setOrigin(getSize() * 0.5f);
         setFillColor(sf::Color::Red);
         setOutlineThickness(3.f);
         setOutlineColor(sf::Color::Black);
         setPosition(START_X_POS, START_Y_POS);
+    }
+
+    void jump() {
+        speed = JUMP_SPEED;
+    }
+
+    bool is_alive() const {
+        if (getPosition().y > static_cast<float>(WINDOW_HEIGHT)) {
+            std::cerr << "Bird is dead\n";
+            return false;
+        }
+        return true;
+    }
+
+    void update(float dt) {
+        if (is_alive()) {
+            time_lived += dt;
+
+            if (getPosition().y < Bird::SIZE * 0.5f) {
+                speed = GRAVITY * 0.04f;
+            }
+
+            /// distance fall influenced by gravity.
+            float distance = (speed * dt) + (0.5f * GRAVITY * dt * dt);
+
+            /// constant acceleration.
+            speed += (GRAVITY * dt);
+
+            move(0.f, distance);
+        }
     }
 };
 
