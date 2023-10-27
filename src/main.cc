17127a41493476bf604ebd2aebd1b1d6f358f312
mrdcvlsc <mrdcvlsc@gmail.com>
2023-10-27 23:19:21 +0800

    added a CLI argument to enable capturing frames

--- src/main.cc
+++ src/main.cc
@@ -3,18 +3,25 @@
 #include <chrono>
 #include <random>
 #include <memory>
+#include <string>
+#include <sstream>
+#include <iomanip>
 
 #include <cmath>
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Image.hpp>
+#include <SFML/Graphics/RenderTexture.hpp>
+#include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/Text.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/System/Time.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Window.hpp>
 #include <SFML/Window/VideoMode.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
 
 #include "bird.hpp"
 #include "config.hpp"
@@ -23,7 +30,17 @@
 #include "collision.hpp"
 #include "genetic_algo.hpp"
 
-int main()
+std::string lpad(std::string const &raw, char fillchar, size_t width)
+{
+    static std::ostringstream ss;
+    ss << std::right << std::setfill(fillchar) << std::setw(width) << raw;
+    std::string padded = ss.str();
+    ss.str(std::string());
+    ss.clear();
+    return padded;
+}
+
+int main(int argc, char *args[])
 {
     auto game_statistics = std::make_shared<GameStats>();
 
@@ -38,6 +55,17 @@ int main()
     sf::RenderWindow window(video_dimension, "Flappy FFNN-GA");
     window.setView(game_view);
     window.setFramerateLimit(GameStats::FRAME_LIMIT);
+
+    sf::Texture capture_texture;
+    size_t      captured_frames = 0;
+    bool        capture_frames = false;
+    if (argc == 2) {
+        if (std::string(args[1]) == "capture") {
+            std::cout << "frame capture flag enabled\n";
+            capture_frames = true;
+            capture_texture.create(WINDOW_WIDTH, WINDOW_HEIGHT);
+        }
+    }
 
     sf::Color background(19, 235, 220);
 
@@ -135,6 +163,13 @@ int main()
         window.draw(*game_statistics);
 
         window.display();
+
+        if (capture_frames) {
+            capture_texture.update(window);
+            capture_texture.copyToImage().saveToFile(
+              "flappy-ffnn-ga-frame-" + lpad(std::to_string(captured_frames++), '0', 6) + ".jpg"
+            );
+        }
 
         if (birds.population == 0ULL && player_bird.dead) {
             genetic_algorithm.rank_fitness(birds);
